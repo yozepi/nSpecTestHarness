8da400dec91267f43f25044c48576e58988f40b4
Joe.Cleland <joe.cleland@neudesic.com>
2017-06-27 16:05:29 -0700

    Added facilities to build a nuget package.

---
+++ Solution/nSpecTestHarness/nSpecTestHarness/NUnit_Harness_Example.cs.pp
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using yozepi;
+
+namespace $rootnamespace$.yozepi
+{
+    //[TestFixture]
+    public class NUnit_Harness_Example : nSpecTestHarness
+    {
+        //A simple example of running specs in NUnit 
+        //[Test]
+        public void TestTheSpecs()
+        {
+            this.RunMySpecs();
+        }
+
+        void sample_spec()
+        {
+            var beautifulDay = true;
+            it["should be a beautiful day"] = () =>
+                Assert.IsTrue(beautifulDay);
+        }
+    }
+}
--- Solution/nSpecTestHarness/nSpecTestHarness/Properties/AssemblyInfo.cs
+++ Solution/nSpecTestHarness/nSpecTestHarness/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("nSpecTestHarness")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Visual Studio test harness for NSpec")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("nSpecTestHarness")]
